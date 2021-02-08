acfae985401869609f7bd4376686c270ce8dbc20
M145508 <christenson.thuy@mayo.edu>
2021-02-08 13:45:53 -0600

    added s3 bucket module

--- main.tf
+++ main.tf
@@ -10,7 +10,8 @@ resource "aws_vpc" "hashicat" {
   tags = {
     name = "${var.prefix}-vpc-${var.region}"
     environment = "Production"
-    department = "devops"
+    Department = "devops"
+    Billable = "devops"
   }
 }
 
